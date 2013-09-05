58109b089ab55af849d118151e26db397fc61b74
Patrick <p.sudhaus2@gmail.com>
2013-09-05 15:09:01 +0200

    Initial Commit
    Signed-off-by: Patrick <p.sudhaus2@gmail.com>

---
+++ gen/com/patsud/melden/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.patsud.melden;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/patsud/melden/Menu.java
@@ -0,0 +1,15 @@
+package com.patsud.melden;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Menu extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.menu);
+	}
+
+
+}
